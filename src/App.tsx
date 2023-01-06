5de9781397e4fa9cc5830c89c548b7cbc1d08edb
Szymon Firlinger <szymon.firlinger@imfactory.com.pl>
2023-01-06 12:34:16 +0100

    🎉 Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Grid, Paper, styled } from '@mui/material';
+import Image from './img/appPhoto.jpg'; // Import using relative path
+
+
+const AppBarPhoto = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  height: "50vh",
+  backgroundSize: "contain",
+  opacity: "50%",
+  backgroundRepeat: "no-repeat",
+  backgroundImage: `url(${Image})`
+}));
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+      >
+        <Grid item >
+          <Paper elevation={8}>
+            <AppBarPhoto />
+            <h1>test</h1>
+          </Paper>
+        </Grid>
+      </Grid>
+
+
+
+
+      {/* 
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: '100vh' }}
+      >
+        <div>
+          <AppBarPhoto />
+        </div>
+        <div>
+          Counter
+        </div>
+        <div>
+          <div>sala</div>
+          <div>kosciol</div>
+        </div>
+        <div>
+          prezenty
+        </div>
+        <div>
+          dane kontaktowe
+        </div>
+        <div>
+          location
+        </div>
+        <div>
+          footer
+        </div>
+
+      </Grid> */}
     </div>
   );
 }
